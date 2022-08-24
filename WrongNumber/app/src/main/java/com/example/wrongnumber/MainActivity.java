f11d4cf7aacd30ce75f5a848d982148e26dace34
haider1122-pgc <70263008+haider1122-pgc@users.noreply.github.com>
2022-08-24 16:15:24 +0500

    started screen implemented

--- WrongNumber/app/src/main/java/com/example/wrongnumber/MainActivity.java
+++ WrongNumber/app/src/main/java/com/example/wrongnumber/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.wrongnumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        //code for hold screen for 1000 milli sec
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openNewAvtivityOnBTNClick();
+            }
+        },2000);
+
+
+
+
+
+
+
+
+
     }
+    private void openNewAvtivityOnBTNClick() {
+        
+        Intent intent = new Intent(this, StartedScreen.class);
+        // intent.putExtra("message",te); //to access the widgets of second activity
+        startActivity(intent);
+
+
+
+    }
+
 }
---
+++ WrongNumber/app/src/main/java/com/example/wrongnumber/StartedScreen.java
@@ -0,0 +1,14 @@
+package com.example.wrongnumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartedScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_started_screen);
+    }
+}
